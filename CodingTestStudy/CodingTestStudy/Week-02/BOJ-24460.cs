9a648ac326d9bd7ac35b5e0c981e74a18b74b0d6
KiStudent <kistudentmeta5@gmail.com>
2024-02-07 12:08:36 +0900

    feat : Upload Second Week

---
+++ CodingTestStudy/CodingTestStudy/Week-02/BOJ-24460.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTestStudy.Week_02
+{
+    internal class BOJ_24460
+    {
+        public void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            if (count == 0)
+                return;
+            int[,] array = new int[count, count];
+            for (int i = 0; i < count; i++)
+            {
+                string[] value = Console.ReadLine().Split(" ");
+                for (int j = 0; j < value.Length; j++)
+                {
+                    array[i, j] = int.Parse(value[j]);
+                }
+            }
+
+            List<int> numTwo = new List<int>();
+
+            Split(array, 0, 0, count, count, numTwo);
+            numTwo.Sort();
+            if (numTwo.Count > 1)
+                Console.WriteLine(numTwo[1]);
+            else
+                Console.WriteLine(numTwo[0]);
+        }
+
+        private void Split(int[,] array, int yPos, int xPos, int ySize, int xSize, List<int> save)
+        {
+            if (ySize > 2)
+            {
+                Split(array, yPos + (ySize >> 1), xPos + (xSize >> 1), ySize >> 1, xSize >> 1, save);
+                Split(array, yPos, xPos + (xSize >> 1), ySize >> 1, xSize >> 1, save);
+                Split(array, yPos + (ySize >> 1), xPos, ySize >> 1, xSize >> 1, save);
+                Split(array, yPos, xPos, ySize >> 1, xSize >> 1, save);
+            }
+            else
+            {
+                List<int> list = new List<int>();
+                for (int i = yPos; i < yPos + ySize; i++)
+                {
+                    for (int j = xPos; j < xPos + xSize; j++)
+                    {
+                        list.Add(array[i, j]);
+                    }
+                }
+                list.Sort();
+                save.Add(list[1]);
+            }
+        }
+    }
+
+}
---
+++ CodingTestStudy/CodingTestStudy/Week-02/BOJ_2630.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTestStudy.Week_02
+{
+    internal class BOJ_2630
+    {
+        public void Main()
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[,] array = new int[count, count];
+            for (int i = 0; i < count; i++)
+            {
+                string[] str = Console.ReadLine().Split(" ");
+                for (int j = 0; j < count; j++)
+                {
+                    array[i, j] = int.Parse(str[j]);
+                }
+            }
+            int white = 0;
+            int blue = 0;
+
+            Check(array, 0, 0, count, count, ref white, ref blue);
+            Console.WriteLine(white);
+
+            Console.WriteLine(blue);
+
+
+        }
+
+        private void Check(int[,] array, int xPos, int yPos, int xSize, int ySize, ref int white, ref int blue)
+        {
+            if (Split(array, xPos, yPos, xSize, ySize))
+            {
+                if (array[xPos, yPos] == 0)
+                    white++;
+                else
+                    blue++;
+                return;
+            }
+            else
+            {
+                Check(array, xPos + (xSize >> 1), yPos + (ySize >> 1), xSize >> 1, ySize >> 1, ref white, ref blue);
+                Check(array, xPos + (xSize >> 1), yPos, xSize >> 1, ySize >> 1, ref white, ref blue);
+                Check(array, xPos, yPos + (ySize >> 1), xSize >> 1, ySize >> 1, ref white, ref blue);
+                Check(array, xPos, yPos, xSize >> 1, ySize >> 1, ref white, ref blue);
+            }
+        }
+
+        private bool Split(int[,] array, int xPos, int yPos, int xSize, int ySize)
+        {
+            int blue = array[xPos, yPos];
+            for (int i = xPos; i < xPos + xSize; i++)
+            {
+                for (int j = yPos; j < yPos + ySize; j++)
+                {
+                    if (blue != array[i, j])
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
